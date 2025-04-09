ddebe279c98d78b8e76efdd561fb5816b2e72d58
chokobar89 <chokobar89@naver.com>
2025-04-09 22:02:15 +0900

    Initial commit

---
+++ src/core/Test07.java
@@ -0,0 +1,17 @@
+package core;
+
+public class Test07 {
+
+    public static void main(String[] args) {
+        /*
+         * 특수문자출력하기
+         *
+         * 문제 설명
+         * 다음과 같이 출력하도록 코드를 작성해 주세요.(!@#$%^&*(\'"<>?:;)
+         *
+         */
+
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+
+    }
+}
